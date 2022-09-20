8fb3fefaf111baf4cdf87e43c0b0ae4fc1a46c7b
Amir Maghami <amaghami@haco.dc>
2022-09-20 17:17:52 +0430

    feat: set path config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import WindiCSS from 'vite-plugin-windicss'
 
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   plugins: [
 
     react(),
@@ -17,3 +24,4 @@ export default defineConfig({
 
   ]
 })
+
